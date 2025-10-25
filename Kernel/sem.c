9a6db893e763821ed4c1d5582da6f95f6533a645
eugemigliaro <eugenio.migliaro@gmail.com>
2025-10-25 15:25:53 -0300

    arrancados los semáforos, falta todavía

--- Kernel/sem.c
+++ Kernel/sem.c
@@ -1,2 +1,27 @@
 #include <sem.h>
+#include <queueADT.h>
+#include <stdint.h>
+#include <lib.h>
 
+typedef struct sem_queue{
+    uint8_t lock;
+    queue_t *semaphores;
+}sem_queue_t;
+
+typedef struct semaphore{
+    char *name;
+    uint32_t value;
+    uint8_t lock;
+    queue_t *waiting_processes;
+}sem_t;
+
+int sem_post(sem_t *sem){
+    int ret = -1;
+    if(sem == NULL){
+        return ret;
+    }
+
+    semLock(&sem->lock);
+    
+    semUnlock(&sem->lock);
+}
