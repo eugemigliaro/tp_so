408d806ddbecac138bd06ce4c7b37a7cebda6e23
sallegretti <santiagoallegretti@gmail.com>
2025-11-05 16:04:42 -0300

    fix: excepciones y rendimiento

--- run.sh
+++ run.sh
@@ -20,8 +20,15 @@ fi
 
 case $OS in
     "Darwin")
-        # macOS - usar CoreAudio
-        AUDIO_CONFIG="-audiodev coreaudio,id=speaker -machine pcspk-audiodev=speaker"
+        # macOS - usar CoreAudio y Hypervisor.framework
+        ACCEL_FLAGS=""
+        if sysctl kern.hv_support 2>/dev/null | grep -q ": 1"; then
+            ACCEL_FLAGS="-accel hvf"
+            echo "Hypervisor.framework disponible - habilitando aceleración por hardware"
+        else
+            echo "Hypervisor.framework no disponible - ejecutando sin aceleración"
+        fi
+        AUDIO_CONFIG="-audiodev coreaudio,id=speaker -machine pcspk-audiodev=speaker $ACCEL_FLAGS"
         echo "Configurando audio para macOS - usando CoreAudio"
         ;;
     "Linux")
@@ -35,19 +42,39 @@ case $OS in
             # Alternativa si has configurado el servidor Pulseaudio en Windows
             # AUDIO_CONFIG="-audiodev pa,id=speaker -machine pcspk-audiodev=speaker"
         else
-            # Linux nativo
+            # Linux nativo - habilitar KVM si está disponible
+            KVM_FLAGS=""
+            if [ -e /dev/kvm ] && [ -r /dev/kvm ] && [ -w /dev/kvm ]; then
+                KVM_FLAGS="-enable-kvm"
+                echo "KVM disponible - habilitando aceleración por hardware"
+            else
+                echo "KVM no disponible - ejecutando sin aceleración"
+            fi
+            
             if command -v pulseaudio >/dev/null 2>&1; then
-                AUDIO_CONFIG="-audiodev pa,id=speaker -machine pcspk-audiodev=speaker"
+                AUDIO_CONFIG="-audiodev pa,id=speaker -machine pcspk-audiodev=speaker $KVM_FLAGS"
                 echo "Configurando audio para Linux con PulseAudio"
             else
-                AUDIO_CONFIG="-audiodev alsa,id=speaker,out.try-poll=on -machine pcspk-audiodev=speaker"
+                AUDIO_CONFIG="-audiodev alsa,id=speaker,out.try-poll=on -machine pcspk-audiodev=speaker $KVM_FLAGS"
                 echo "Configurando audio para Linux - usando ALSA"
             fi
         fi
         ;;
     CYGWIN*|MINGW*|MSYS*)
         # Windows nativo con Cygwin/MinGW/MSYS
-        AUDIO_CONFIG="-audiodev dsound,id=speaker -machine pcspk-audiodev=speaker"
+        # Intentar WHPX (Windows Hypervisor Platform) o HAXM
+        ACCEL_FLAGS=""
+        # WHPX requiere Windows 10/11 con Hyper-V
+        if qemu-system-x86_64 -accel help 2>/dev/null | grep -q "whpx"; then
+            ACCEL_FLAGS="-accel whpx"
+            echo "WHPX disponible - habilitando aceleración por hardware"
+        elif qemu-system-x86_64 -accel help 2>/dev/null | grep -q "hax"; then
+            ACCEL_FLAGS="-accel hax"
+            echo "HAXM disponible - habilitando aceleración por hardware"
+        else
+            echo "Aceleración no disponible - ejecutando sin aceleración"
+        fi
+        AUDIO_CONFIG="-audiodev dsound,id=speaker -machine pcspk-audiodev=speaker $ACCEL_FLAGS"
         echo "Configurando audio para Windows - usando DirectSound"
         ;;
     *)
